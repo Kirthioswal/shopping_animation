04c1cdfce334444888651a8434e8dfd8ec670dd9
Anupam <anupam@whitebrands.com>
2025-08-21 01:37:10 +0530

    section added

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
 import PlaySection from "@/components/PlaySection";
+import AutomationSection from "@/components/AutomationSection";
 import TeamSection from "@/components/TeamSection";
 import ProcessSection from "@/components/ProcessSection";
 import AITeamSection from "@/components/AITeamSection";
@@ -17,6 +18,7 @@ const Index = () => {
       {/* Ensure enough scrollable content for video animation */}
       <div className="relative z-20 bg-background">
         <PlaySection />
+        <AutomationSection />
         <TeamSection />
         <ProcessSection />
         <AITeamSection />
